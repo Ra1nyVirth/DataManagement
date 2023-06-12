c39e3b38fe8d6304bbf03685d3558912ff7b9ca5
s1300017 <s1300017@u-aizu.ac.jp>
2023-06-12 12:33:46 +0900

    “Dice game: initial revision”.

---
+++ DiceGame.py
@@ -0,0 +1,12 @@
+import random
+
+die1 = random.randint(1, 6)
+die2 = random.randint(1, 6)
+
+sum = die1 + die2
+
+print("Rolling dice...")
+print("Die1: ", die1)
+print("Die2: ", die2)
+print("Total value:", sum)
+
